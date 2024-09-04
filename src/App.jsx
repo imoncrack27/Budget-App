b6f6565c3c0eea74bbe8c83e39a915c29fc1315e
imoncrack27 <mingodrexell321@gmail.com>
2024-09-04 21:13:27 +0800

    implemented a logout for user

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
+
+//routes
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
@@ -6,9 +14,21 @@ import Error from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
   },
 ]);
 
