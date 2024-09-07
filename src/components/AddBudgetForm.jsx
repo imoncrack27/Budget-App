5a3e6518d5de3cb715f8a1517c2e76932c434c47
imoncrack27 <mingodrexell321@gmail.com>
2024-09-07 21:37:55 +0800

    added a useFetcher hook

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,13 +1,14 @@
-import { Form } from "react-router-dom";
+import { Form, useFetcher } from "react-router-dom";
 
 //library imports
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid";
 
 function AddBudgetForm() {
+  const fetcher = useFetcher();
   return (
     <div className="form-wrapper">
       <h2 className="h3">Create budget</h2>
-      <Form method="post" className="grid-sm">
+      <fetcher.Form method="post" className="grid-sm">
         <div className="grid-xs">
           <label htmlFor="newBudget">Budget Name</label>
           <input
@@ -35,7 +36,7 @@ function AddBudgetForm() {
           <span>Create budget</span>
           <CurrencyDollarIcon width={20} />
         </button>
-      </Form>
+      </fetcher.Form>
     </div>
   );
 }
