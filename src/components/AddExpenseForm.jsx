3943ec5c5058b2b808541afd017caa6a21e3d42f
imoncrack27 <mingodrexell321@gmail.com>
2024-09-14 23:47:01 +0800

    added fetcher.form on new expense form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+//react imports
+import { useRef } from "react";
 
-const AddExpenseForm = () => {
+//rrd imports
+import { useFetcher } from "react-router-dom";
+
+const AddExpenseForm = ({ budgets }) => {
+  const fetcher = useFetcher();
+
+  const formRef = useRef();
+  const focusRef = useRef();
+
   return (
     <div className="form-wrapper">
       <h2 className="h3">
-        Add New{" "}
+        Add New {""}
         <span className="accent">
           {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
-        </span>
+        </span>{" "}
+        Expense
       </h2>
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
+        <div className="expense-inputs">
+          <div className="grid-xs">
+            <label htmlFor="newExpense">Expense Name</label>
+            <input
+              type="text"
+              name="newExpense"
+              id="newExpense"
+              placeholder="e.g., Coffee"
+              ref={focusRef}
+              required
+            />
+          </div>
+          <div className="grid-xs">
+            <label htmlFor="newExpenseAmount">Amount</label>
+            <input
+              type="number"
+              step="0.01"
+              inputMode="decimal"
+              name="newExpenseAmount"
+              id="newExpenseAmount"
+              placeholder="e.g., 3.50"
+              required
+            />
+          </div>
+        </div>
+      </fetcher.Form>
     </div>
   );
 };
